b8389d7f8a492a270da62757dd8acfb28f5fcdb6
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2024-03-02 10:45:08 +0100

    Add func unique() + passing tests

--- furcoat/checks.py
+++ furcoat/checks.py
@@ -1,11 +1,20 @@
+from typing import Iterable
+
 import polars as pl
+from polars.type_aliases import IntoExpr
 
 
 class PolarsCheckError(Exception):
     pass
 
 
-def has_no_nulls(data: pl.DataFrame):
+def has_no_nulls(data: pl.DataFrame) -> pl.DataFrame:
     if data.null_count().sum_horizontal().item() > 0:
+        raise PolarsCheckError
+    return data
+
+
+def unique(data: pl.DataFrame, columns: IntoExpr | Iterable[IntoExpr]) -> pl.DataFrame:
+    if data.select(columns).is_duplicated().any():
         raise PolarsCheckError
     return data
--- tests/test_checks.py
+++ tests/test_checks.py
@@ -6,20 +6,33 @@ from furcoat import checks
 
 
 def test_has_no_nulls_returns_df_when_all_values_defined():
-    given = pl.DataFrame(
-        {
-            "a": [1, 2],
-        }
-    )
+    given = pl.DataFrame({"a": [1, 2]})
     when = given.pipe(checks.has_no_nulls)
     testing.assert_frame_equal(given, when)
 
 
 def test_has_no_nulls_throws_error_on_null_values():
-    given = pl.DataFrame(
-        {
-            "a": [1, None],
-        }
-    )
+    given = pl.DataFrame({"a": [1, None]})
     with pytest.raises(checks.PolarsCheckError):
         given.pipe(checks.has_no_nulls)
+
+
+def test_unique_should_return_df_if_column_has_unique_values():
+    given = pl.DataFrame({"a": [1, 2]})
+    when = given.pipe(checks.unique, "a")
+    testing.assert_frame_equal(given, when)
+
+
+def test_unique_should_should_accept_list_and_polars_select():
+    given = pl.DataFrame({"a": [1, 2]})
+    when = given.pipe(checks.unique, ["a"])
+    testing.assert_frame_equal(given, when)
+    given = pl.DataFrame({"a": [1, 2]})
+    when = given.pipe(checks.unique, pl.col("a"))
+    testing.assert_frame_equal(given, when)
+
+
+def test_unique_should_throw_error_on_duplicates():
+    given = pl.DataFrame({"a": [1, 1]})
+    with pytest.raises(checks.PolarsCheckError):
+        given.pipe(checks.unique, "a")
