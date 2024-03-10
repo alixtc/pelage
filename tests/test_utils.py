1b8b7321c9a2c78f2ae6608781a4882bf9d5f0fb
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2024-03-10 08:20:27 +0100

    Add function to compare df schemas

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 import polars as pl
 
 from pelage import utils
@@ -24,3 +26,27 @@ def test_list_defective_columns_should_return_all_name_of_defective_columns():
     not_met_conditions = [cond.not_() for cond in check]
     bad = given.filter(pl.Expr.or_(*not_met_conditions))
     assert utils._list_defective_columns(bad, not_met_conditions) == ["a", "b"]
+
+
+def test_compare_schema_should_be_empty_when_types_matche():
+    given = pl.DataFrame({"a": [1, 2], "b": [1.0, 1.2]})
+    expected_schema = OrderedDict(a=pl.Int64, b=pl.Float64)
+    assert utils.compare_schema(given.schema, expected_schema) == ""
+
+
+def test_compare_schema_returns_column_expected_and_real_dtypes():
+    given = pl.DataFrame({"a": [1, 2], "b": [1.0, 1.2]})
+    expected_schema = OrderedDict(a=pl.Int64, b=pl.Int64)
+    message = "column='b', expected_type=Int64, real_dtype=Float64"
+    assert utils.compare_schema(given.schema, expected_schema) == message
+
+
+def test_compare_schema_returns_one_line_per_mismatch_column():
+    given = pl.DataFrame({"a": [1, 2], "b": [1.0, 1.2]})
+    expected_schema = OrderedDict(a=pl.Float32, b=pl.Int64)
+    message = (
+        "column='a', expected_type=Float32, real_dtype=Int64"
+        + "\n"
+        + "column='b', expected_type=Int64, real_dtype=Float64"
+    )
+    assert utils.compare_schema(given.schema, expected_schema) == message
