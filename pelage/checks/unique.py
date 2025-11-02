72e1e71e6fcd29e2ba23ea5ee56f553496e805eb
alixtc <alix.tiran-cappello@laposte.net>
2025-11-02 21:29:33 +0100

    feat(unique): add group_by option

--- pelage/checks/unique.py
+++ pelage/checks/unique.py
@@ -6,6 +6,7 @@ from pelage.types import (
     PolarsAssertError,
     PolarsColumnType,
     PolarsLazyOrDataFrame,
+    PolarsOverClauseInput,
 )
 from pelage.utils import _sanitize_column_inputs
 
@@ -13,11 +14,12 @@ from pelage.utils import _sanitize_column_inputs
 def unique(
     data: PolarsLazyOrDataFrame,
     columns: Optional[PolarsColumnType] = None,
+    group_by: Optional[PolarsOverClauseInput] = None,
 ) -> PolarsLazyOrDataFrame:
     """Check if there are no duplicated values in each one of the selected columns.
 
-    This is a column oriented check, for a row oriented check see
-    `unique_combination_of_columns`
+    This is a column oriented check, where each columns are search independently for
+    duplicated values. For a row oriented check see `unique_combination_of_columns`
 
     Parameters
     ----------
@@ -64,10 +66,61 @@ def unique(
     └─────┘
     Error with the DataFrame passed to the check function:
     --> Somes values are duplicated within the specified columns
+
+    >>> df = pl.DataFrame(
+    ...     [
+    ...         [1, 1, 1],
+    ...         [1, 1, 2],
+    ...     ],
+    ...     schema=["col1", "col2", "group"],
+    ...     orient="row",
+    ... )
+    >>> df.pipe(plg.unique, ["col1", "col2"], group_by="group")
+    shape: (2, 3)
+    ┌──────┬──────┬───────┐
+    │ col1 ┆ col2 ┆ group │
+    │ ---  ┆ ---  ┆ ---   │
+    │ i64  ┆ i64  ┆ i64   │
+    ╞══════╪══════╪═══════╡
+    │ 1    ┆ 1    ┆ 1     │
+    │ 1    ┆ 1    ┆ 2     │
+    └──────┴──────┴───────┘
+
+    >>> df = pl.DataFrame(
+    ...     [
+    ...         [1, 1, 1],
+    ...         [1, 1, 1],
+    ...         [1, 1, 2],
+    ...     ],
+    ...     schema=["col1", "col2", "group"],
+    ...     orient="row",
+    ... )
+    >>> df.pipe(plg.unique, ["col1", "col2"], group_by="group")
+    Traceback (most recent call last):
+    ...
+    pelage.types.PolarsAssertError: Details
+    shape: (2, 3)
+    ┌──────┬──────┬───────┐
+    │ col1 ┆ col2 ┆ group │
+    │ ---  ┆ ---  ┆ ---   │
+    │ i64  ┆ i64  ┆ i64   │
+    ╞══════╪══════╪═══════╡
+    │ 1    ┆ 1    ┆ 1     │
+    │ 1    ┆ 1    ┆ 1     │
+    └──────┴──────┴───────┘
+    Error with the DataFrame passed to the check function:
+    --> Somes values are duplicated within the specified columns
     """
     selected_cols = _sanitize_column_inputs(columns)
+    highlight_columns_with_duplication = (
+        selected_cols.is_duplicated()
+        if group_by is None
+        else selected_cols.is_duplicated().over(group_by)
+    )
     improper_data = (
-        data.lazy().filter(pl.any_horizontal(selected_cols.is_duplicated())).collect()
+        data.lazy()
+        .filter(pl.any_horizontal(highlight_columns_with_duplication))
+        .collect()
     )
 
     if not improper_data.is_empty():
